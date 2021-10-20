0d24f30a38beebf4e81860ec95c2f4de5338402a
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2021-10-20 22:18:04 +0530

    Implemented Layout

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(expressLayouts);
 
+// use express router
 app.use('/', require('./routes'));
+
+// set up view engine
 app.set('view engine','ejs');
 app.set('views','./views');
 
