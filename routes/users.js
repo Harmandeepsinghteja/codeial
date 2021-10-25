875156a1765d09e915783ebcadce6f40de0cacd0
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2021-10-25 22:07:38 +0530

    User Sign Up

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,6 @@ const usersControllers = require('../controllers/users_controllers')
 router.get('/profile',usersControllers.profile)
 router.get('/sign-up', usersControllers.signUp);
 router.get('/sign-in',usersControllers.signIn);
+router.post('/create',usersControllers.create)
 module.exports = router;
 
