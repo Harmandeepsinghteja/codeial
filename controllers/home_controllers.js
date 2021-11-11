6ea7f8540a581387e6c2be2dbabb40023a60bad8
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2021-11-11 22:05:34 +0530

    Display Post and related user

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,5 +1,20 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    return res.render('home',{
-        title:"Home"
+    
+    
+    // Post.find({user:req.user._id}, function(err,posts){
+    //     return res.render('home',{
+    //         title:"Home",
+    //         posts:posts
+    //     })
+    // })
+
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Home",
+            posts:posts
+        })
     })
+    
 };
