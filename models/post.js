21a7269b723b5df645588423f78baf59f8df74d0
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2021-11-11 22:27:55 +0530

    Creating Schema for Comments

--- models/post.js
+++ models/post.js
@@ -9,7 +9,14 @@ const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    // include the array of ids of all the comments in this schema otself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'comment'
+        }
+    ]
 },{
     timestamps: true
 });
