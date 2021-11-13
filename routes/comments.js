dc5c01b54d8ba98c5d20b115b7d96d958be8d9ef
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2021-11-13 17:28:48 +0530

    Deleting a Comment

--- routes/comments.js
+++ routes/comments.js
@@ -4,5 +4,5 @@ const commentsControllers = require('../controllers/comments_controllers');
 const passport = require('passport');
 
 router.post('/create',passport.checkAuthentication,commentsControllers.create);
-
+router.get('/destroy/:id', passport.checkAuthentication, commentsControllers.destroy)
 module.exports = router;
