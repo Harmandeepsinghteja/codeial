d9bbb1381a735b36b7efdd1a1d220b436884ae19
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2021-11-21 13:28:10 +0530

    More Noty Js

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -5,15 +5,18 @@ const User = require('../models/user');
 
 // authentication using passport
 passport.use(new LocalStrategy({
-    usernameField: 'email'
-}, function(email,password,done){
+    usernameField: 'email',
+    passReqToCallback:true
+}, function(req,email,password,done){
     // find the user and establish the identity
     User.findOne({email:email}, function(err,user){
         if(err){
+            req.flash('error', err);
             console.log("Error in finding the user --> Passport");
             return done(err);
         }
         if(!user || user.password!=password){
+            req.flash('error', 'Invalid Username/Password');
             console.log("Invalid Username/Password");
             return done(null,false);
         }
