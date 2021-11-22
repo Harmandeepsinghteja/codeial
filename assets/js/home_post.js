a906e5ca0408b493af654904200e74290ce3243f
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2021-11-22 22:24:01 +0530

    Using Ajax for creating Post

---
+++ assets/js/home_post.js
@@ -0,0 +1,56 @@
+{
+    // method to submit the form data for new post using Ajax
+    let createPost = function(){
+        let newPostForm = $('#new-post-form');
+
+        newPostForm.submit(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type:'post',
+                url:'/posts/create',
+                data: newPostForm.serialize(),
+                success: function(data){
+        
+                    let newPost = newPostDom(data.data.post);
+                    $('#post-list-container').prepend(newPost);
+                }, error: function(error){
+                    console.log(error.responseText);
+                }
+            })
+        })
+    }
+
+    // method to create a post in DOM
+    let newPostDom = function(post){
+        return $(`
+        <li id="post-<%= ${post._id} %>">
+        
+                
+            <small> <a class="delete-post-button"  href="/posts/destroy/<%= ${post.id} %>">Delete</a></small>
+            
+            <li> <%= ${post.content} %> </li>
+            <p><small> <%= ${post.user.name} %> </small> </p>
+            <div class="post-comments">
+                  
+                            <form action="/comments/create" method="POST">
+                                    <input type="text" name="commentContent" required placeholder="Type Here.." >
+                                    <input type="hidden" name="post" value="<%= ${post._id} %>">
+                                    <input type="submit" value="Comment">      
+                            </form>
+                 
+                    <div class="post-comments-list">
+                            <ul id ="post-comments-<%= ${post._id} %>"></ul>
+                                    
+                    </div>
+            </div>
+        
+        </li>`)
+    }
+
+
+
+
+
+    createPost();
+}
