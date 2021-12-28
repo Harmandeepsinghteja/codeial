b88165a708492e3a6e7e2cd0ed0c17ca688a7791
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2021-12-28 22:14:54 +0530

    Getting Production ready

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const env = require('./config/environment')
 const cookieParser = require('cookie-parser');
 const app = express()
+require('./config/view-helpers')(app);
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const morgan  = require('morgan');
